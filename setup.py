04bf557b47d7080fec91c5e4aba4a5ce061293d2
syawar <you@example.com>
2016-05-27 14:22:30 -0400

    Addiing support for push to pypi

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
         name='pentaho-rest-api',
 
-        version='1.0.11',
+        version='1.0.12',
 
         include_package_data=True,
 
