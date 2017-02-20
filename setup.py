2d28e89f0b521f3ecae1d223ed293d80cb161318
Pierre Leca <pierre.leca@alayacare.com>
2017-02-20 15:38:10 -0500

    update version to 1.0.25

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
         name='pentaho-rest-api',
 
-        version='1.0.24',
+        version='1.0.25',
 
         include_package_data=True,
 
